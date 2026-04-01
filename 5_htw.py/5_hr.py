d37a760e6e842f30dd6f434d671351c1293cda6b
r4harov <162058273+r4harov@users.noreply.github.com>
2026-04-02 01:15:24 +0300

    Add files via upload

---
+++ 5_htw.py/5_hr.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+driver = webdriver.Chrome()
+driver.get("https://www.saucedemo.com")
+driver.find_element(By.ID, "user-name").send_keys("standard_user")
+driver.find_element(By.ID, "password").send_keys("secret_sauce")
+driver.find_element(By.ID, "login-button").click()
+print("Все нужные элементы найдены")
+driver.quit()
